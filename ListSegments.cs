39854d368c334beeab32e7c9666c9fce91185fae
sneko <supernekocat31@gmail.com>
2023-12-12 21:24:04 +0900

    add ls program.

---
+++ ListSegments.cs
@@ -0,0 +1,26 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+class ListSegments {
+    static void Main(string[] args)
+    {
+        // パス取得
+        string path = getPath();
+        // 指定されたディレクトリ内のファイルとディレクトリを取得
+        IEnumerable<string> segments = Directory.EnumerateFileSystemEntries(path);
+        // ファイルとディレクトリを表示
+        foreach (string segment in segments) {
+            Console.WriteLine(segment);
+        }
+    }
+
+    static string getPath()
+    {
+        // コマンドライン引数を配列で取得する
+        string[] cmds = Environment.GetCommandLineArgs();
+        // コマンドライン引数があればそれを、なければ現在のディレクトリを取得
+        return (cmds.Length > 1) ? cmds[1] : Directory.GetCurrentDirectory();
+    }
+}
