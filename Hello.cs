21292396b72c76cee5878a3fd7ccd0913e2b4563
sneko <supernekocat31@gmail.com>
2023-12-18 23:11:25 +0900

    fix and studied List.

--- Hello.cs
+++ Hello.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Runtime.InteropServices;
+using System.Collections.Generic;
 
 class Hello
 {
@@ -9,7 +10,15 @@ class Hello
         Console.WriteLine(RuntimeInformation.FrameworkDescription);
 
         int[] nums = new int[]{10, 11, 12};
-        foreach (int num in nums) {
+        foreach (int num in nums)
+        {
+            Console.Write(num + " ");
+        }
+
+        List<int> list = new List<int>(){13, 14};
+        list.Add(15);
+        foreach (int num in list)
+        {
             Console.Write(num + " ");
         }
         Console.WriteLine();
--- ListSegments.cs
+++ ListSegments.cs
@@ -3,8 +3,8 @@ using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 
-class ListSegments {
-
+class ListSegments
+{
     /**
      * メイン処理
      */
