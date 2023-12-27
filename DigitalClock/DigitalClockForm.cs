aeb4aab9be7b35c080914896b4b26e09665a5f78
sneko <supernekocat31@gmail.com>
2023-12-28 00:23:37 +0900

    Studied Datetime.

---
+++ DigitalClock/DigitalClockForm.cs
@@ -0,0 +1,30 @@
+﻿namespace DigitalClock
+{
+    using System.Windows.Forms;
+
+    partial class DigitalClockForm : Form
+    {
+        private Timer timer;
+        private Label label;
+
+        public DigitalClockForm()
+        {
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Start();
+
+            label = new Label();
+            label.Font = new Font("Courier", 15, FontStyle.Regular);
+            label.Dock = DockStyle.Fill;
+            label.Parent = this;
+
+            timer.Tick += new EventHandler(timer_Tick!);
+        }
+
+        private void timer_Tick(Object sender, EventArgs e)
+        {
+            DateTime datetime = DateTime.Now;
+            label.Text = datetime.ToString();
+        }
+    }
+}
---
+++ DigitalClock/Program.cs
@@ -0,0 +1,12 @@
+namespace DigitalClock
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new DigitalClockForm());
+        }
+    }
+}
