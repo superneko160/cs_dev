c5c79b66b9e142e45c6b440cb6c2f2f180361b89
sneko <supernekocat31@gmail.com>
2023-12-27 23:13:38 +0900

    studied Fileinfo and Dialog.

---
+++ FileReader/ExForm.cs
@@ -0,0 +1,46 @@
+﻿namespace FileReader
+{
+    class ExForm : Form
+    {
+        private Button button;
+        private Label[] labels = new Label[3];
+
+        public ExForm()
+        {
+            this.Text = "SAMPLE";
+            this.Width = 700;  // 幅が狭いとフルパス表示できない
+            this.Height = 240;
+
+            for (int i = 0; i < labels.Length; i++)
+            {
+                labels[i] = new Label();
+                labels[i].Top = i * labels[0].Height;
+                labels[i].Width = this.Width;
+            }
+            button = new Button();
+            button.Height = 35;
+            button.Text = "表示";
+            button.Dock = DockStyle.Bottom;
+            button.Parent = this;
+
+            for (int i = 0; i < labels.Length; i++)
+            {
+                labels[i].Parent = this;
+            }
+
+            button.Click += new EventHandler(bt_Click!);
+        }
+
+        public void bt_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                FileInfo fileinfo = new FileInfo(ofd.FileName);
+                labels[0].Text = "ファイル名：" + fileinfo.Name;
+                labels[1].Text = "絶対パス：" + Path.GetFullPath(ofd.FileName);
+                labels[2].Text = "サイズ：" + Convert.ToString(fileinfo.Length);
+            }
+        }
+    }
+}
---
+++ FileReader/Program.cs
@@ -0,0 +1,12 @@
+namespace FileReader
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new ExForm());
+        }
+    }
+}
