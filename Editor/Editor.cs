4077c2cc9bd0d020f8a8a223c3fdba75664fcc92
sneko <supernekocat31@gmail.com>
2023-12-30 00:02:09 +0900

    Add save_dialog_event and button.

--- Editor/Editor.cs
+++ Editor/Editor.cs
@@ -4,13 +4,11 @@
     {
         private TextBox textbox;
         private ToolStrip toolStrip;
-        private ToolStripButton[] tsButton = new ToolStripButton[5];
-        private String fileFilter;
+        private ToolStripButton[] tsButton = new ToolStripButton[6];
+        private string fileFilter;
         internal static readonly string[] fileFilterData = new string[]
             {
                 "テキスト文書|*.txt;*.text",
-                "タブ区切り値|*.tsv",
-                "カンマ区切り値|*.csv",
                 "ログファイル|*.log"
             };
 
@@ -35,12 +33,14 @@
             tsButton[2].Text = "Paste";
             tsButton[3].Text = "Load";
             tsButton[4].Text = "Save";
+            tsButton[5].Text = "Save as";
             // ツールチップテキスト
             tsButton[0].ToolTipText = "切り取り";
             tsButton[1].ToolTipText = "コピー";
             tsButton[2].ToolTipText = "貼り付け";
             tsButton[3].ToolTipText = "ファイル読み込み";
-            tsButton[4].ToolTipText = "ファイル保存";
+            tsButton[4].ToolTipText = "上書き保存";
+            tsButton[5].ToolTipText = "新規保存";
             // ツールチップテキストの追加
             toolStrip = new ToolStrip();
             for (int i = 0; i < tsButton.Length; i++)
@@ -61,26 +61,34 @@
             {
                 tsButton[i].Click += new EventHandler(basic_event!);
             }
-            // 読み込み、書き込みボタンのイベント追加
-            for (int i = 3;  i < tsButton.Length; i++)
+            // 読み込み、上書き保存ボタンのイベント追加
+            for (int i = 3;  i < 5; i++)
             {
                 tsButton[i].Click += new EventHandler(dialog_event!);
+            }
+            // 新規保存ボタンのイベント追加
+            for (int i = 5; i < tsButton.Length; i++)
+            {
+                tsButton[i].Click += new EventHandler(save_dialog_event!);
             }
         }
 
         /**
          * 基本処理ボタンのイベント処理
          */
-        public void basic_event(Object sender, EventArgs e)
+        public void basic_event(object sender, EventArgs e)
         {
+            // 切り取り
             if (sender == tsButton[0])
             {
                 textbox.Cut();
             }
+            // コピー
             else if (sender == tsButton[1])
             {
                 textbox.Copy();
             }
+            // 貼り付け
             else if (sender == tsButton[2])
             {
                 textbox.Paste();
@@ -90,25 +98,45 @@
         /**
          * ファイルダイアログを表示するボタンのイベント処理
          */
-        public void dialog_event(Object sender, EventArgs e)
+        public void dialog_event(object sender, EventArgs e)
         {
             OpenFileDialog dialog = new OpenFileDialog();
             dialog.Filter = fileFilter;
 
             if (dialog.ShowDialog() == DialogResult.OK)
             {
+                // ファイル読み込み
                 if (sender == tsButton[3])
                 {
                     StreamReader streamReader = new StreamReader(dialog.FileName, System.Text.Encoding.Default);
                     textbox.Text = streamReader.ReadToEnd();
                     streamReader.Close();
                 }
-                // 既存ファイルで保存（新規作成で保存は未実装）
+                // 既存ファイルに上書き保存
                 else if (sender == tsButton[4])
                 {
                     StreamWriter streamWriter = new StreamWriter(dialog.FileName);
                     streamWriter.WriteLine(textbox.Text);
                     streamWriter.Close();
+                }
+            }
+        }
+
+        /**
+         * 新規保存ダイアログを表示するボタンのイベント処理
+         */
+        public void save_dialog_event(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = fileFilter;
+            dialog.RestoreDirectory = true;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                // ファイルに書き込み
+                using (StreamWriter streamWriter = new StreamWriter(dialog.FileName))
+                {
+                    streamWriter.Write(textbox.Text);
                 }
             }
         }
