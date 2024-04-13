1d11734f81d4bf0fbb41925a5ff257ba7c1d23da
sneko <supernekocat31@gmail.com>
2024-04-13 20:42:58 +0900

    refactor:divide function GetAllChars(#1)

--- PasswordGenerator/Program.cs
+++ PasswordGenerator/Program.cs
@@ -2,12 +2,8 @@
 {
     static void Main(string[] args)
     {
-        char[] chars = [
-            'a', 'b', 'c', 'd', 'e', 'f',
-            'A', 'B', 'C', 'D', 'E', 'F',
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-            '+', '-', '*', '/', '%', '?', '&', '_'
-        ];
+        // パスワード生成に利用できる文字群を取得
+        char[] chars = GetAllChars();
 
         // Guid.NewGuid().GetHashCode() 生成される乱数の順序が毎回異なるように設定
         Random rand = new Random(Guid.NewGuid().GetHashCode());
@@ -25,6 +21,38 @@
         {
             Console.WriteLine("Invalid password length. Please enter a positive integer.");
         }
+    }
+
+    /// <summary>
+    /// 小文字アルファベット、大文字アルファベット、数字、および一般的な記号を含む配列を取得
+    /// </summary>
+    /// <returns>パスワードに利用可能な文字が含まれる配列</returns>
+    public static char[] GetAllChars()
+    {
+        List<char> charList = new List<char>();
+
+        // 小文字のアルファベット
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            charList.Add(c);
+        }
+
+        // 大文字のアルファベット
+        for (char c = 'A'; c <= 'Z'; c++)
+        {
+            charList.Add(c);
+        }
+
+        // 数字
+        for (char c = '0'; c <= '9'; c++)
+        {
+            charList.Add(c);
+        }
+
+        // 記号
+        charList.AddRange(new[] { '+', '-', '*', '/', '%', '?', '&', '_' });
+
+        return charList.ToArray();
     }
 
     /// <summary>
